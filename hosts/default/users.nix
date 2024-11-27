04a5ec1f49de1cc07777ffe1b0d6ce7b67a75b8c
Mathipe98 <mathias.pettersen@proton.me>
2024-11-27 13:10:32 +0100

    Added user packages

--- hosts/default/users.nix
+++ hosts/default/users.nix
@@ -22,41 +22,19 @@ in
 
     # define user packages here
     packages = with pkgs; [
+	oh-my-posh
+	gh
+	teams
+	slack
+	protonvpn-gui
+	python3Full
+	vscode
       ];
     };
     
-    defaultUserShell = pkgs.zsh;
+    defaultUserShell = pkgs.bash;
   }; 
   
-  environment.shells = with pkgs; [ zsh ];
+  environment.shells = with pkgs; [ bash ];
   environment.systemPackages = with pkgs; [ fzf ]; 
-    
-  programs = {
-  # Zsh configuration
-	  zsh = {
-    	enable = true;
-	  	enableCompletion = true;
-      ohMyZsh = {
-        enable = true;
-        plugins = ["git"];
-        theme = "xiong-chiamiov-plus"; 
-      	};
-      
-      autosuggestions.enable = true;
-      syntaxHighlighting.enable = true;
-      
-      promptInit = ''
-        fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc
-        
-        #pokemon colorscripts like. Make sure to install krabby package
-        #krabby random --no-mega --no-gmax --no-regional --no-title -s; 
-        
-        source <(fzf --zsh);
-        HISTFILE=~/.zsh_history;
-        HISTSIZE=10000;
-        SAVEHIST=10000;
-        setopt appendhistory;
-        '';
-      };
-   };
 }
