ca04a06a38847aacab9235d58cda703551d7c218
Mathipe98 <mathias.pettersen@proton.me>
2024-11-27 15:12:28 +0100

    Added back zsh config

--- hosts/default/users.nix
+++ hosts/default/users.nix
@@ -46,4 +46,33 @@ in
   
   environment.shells = with pkgs; [ zsh ];
   environment.systemPackages = with pkgs; [ fzf ]; 
+
+  programs = {
+  # Zsh configuration
+      zsh = {
+    	enable = true;
+	  	enableCompletion = true;
+      ohMyZsh = {
+        enable = true;
+        plugins = ["git"];
+        theme = "xiong-chiamiov-plus"; 
+      	};
+      
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      
+      promptInit = ''
+        fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc
+        
+        #pokemon colorscripts like. Make sure to install krabby package
+        #krabby random --no-mega --no-gmax --no-regional --no-title -s; 
+        
+        source <(fzf --zsh);
+        HISTFILE=~/.zsh_history;
+        HISTSIZE=10000;
+        SAVEHIST=10000;
+        setopt appendhistory;
+        '';
+      };
+   };
 }
