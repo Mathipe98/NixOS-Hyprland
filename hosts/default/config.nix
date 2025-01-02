1e2aa0b2ddf64fce1f68491d1640f51810f167f4
Mathipe98 <mathias.pettersen@proton.me>
2025-01-02 19:56:19 +0100

    Added keyring + avahi

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -332,25 +332,25 @@
       autodetect = true;
     };
     
-	  gvfs.enable = true;
-	  tumbler.enable = true;
-
-	  pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-	    wireplumber.enable = true;
-  	  };
-	
-	  udev.enable = true;
-	  envfs.enable = true;
-	  dbus.enable = true;
-
-	  fstrim = {
+    gvfs.enable = true;
+    tumbler.enable = true;
+  
+    pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+      wireplumber.enable = true;
+    };
+  
+    udev.enable = true;
+    envfs.enable = true;
+    dbus.enable = true;
+  
+    fstrim = {
       enable = true;
       interval = "weekly";
-      };
+    };
   
     libinput.enable = true;
 
@@ -365,11 +365,11 @@
   	#hardware.openrgb.enable = true;
   	#hardware.openrgb.motherboard = "amd";
 
-	  fwupd.enable = true;
+	fwupd.enable = true;
 
-	  upower.enable = true;
+	upower.enable = true;
     
-    gnome.gnome-keyring.enable = false;
+    gnome.gnome-keyring.enable = true;
     
     #printing = {
     #  enable = false;
@@ -378,11 +378,11 @@
     #  ];
     #};
     
-    #avahi = {
-    #  enable = true;
-    #  nssmdns4 = true;
-    #  openFirewall = true;
-    #};
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
     
     #ipp-usb.enable = true;
     
