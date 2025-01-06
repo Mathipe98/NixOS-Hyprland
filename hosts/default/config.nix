b3ecbe7cb60aa454ade52b966bd0f38b595e14e4
Mathipe98 <mathias.pettersen@proton.me>
2025-01-06 12:08:16 +0100

    Removed buggy env

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -519,5 +519,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-  env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];  };
 }
